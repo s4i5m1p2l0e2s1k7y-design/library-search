6cf7718c895263a0d399d00b01bfcd3bbd4694b5
Simple User <s4i5m1p2l0e2s1k7y@gmail.com>
2026-04-26 11:19:11 +0900

    Fix API route - use CommonJS syntax

--- api/search.js
+++ api/search.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
     const { keyword } = req.query;
 
     if (!keyword) {
@@ -22,4 +22,4 @@ export default async function handler(req, res) {
     } catch (error) {
         return res.status(500).json({ error: '検索に失敗しました: ' + error.message });
     }
-}
+};
